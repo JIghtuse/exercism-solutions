ce9f5dd0bdc0cb4fed19decc228df5404376ebce
Boris Egorov <egorov@linux.com>
2015-12-02 15:01:54 +0600

    cpp/bob: Pass test 6 (forceful questions)

--- cpp/bob/bob.cpp
+++ cpp/bob/bob.cpp
@@ -1,9 +1,14 @@
 #include "bob.h"
+#include <algorithm>
 
 using namespace std;
 
+bool is_shouting(const string& s) {
+    return find_if(s.begin(), s.end(), ptr_fun<int, int>(islower)) == s.end();
+}
+
 const string bob::hey(const string& msg) {
-    if (msg == "WATCH OUT!")
+    if (is_shouting(msg))
         return "Whoa, chill out!";
     if (msg == "Does this cryogenic chamber make me look fat?")
         return "Sure.";
--- cpp/bob/bob_test.cpp
+++ cpp/bob/bob_test.cpp
@@ -27,12 +27,12 @@ BOOST_AUTO_TEST_CASE(using_acronyms_in_regular_speech)
     BOOST_REQUIRE_EQUAL("Whatever.", bob::hey("It's OK if you don't want to go to the DMV."));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(forceful_questions)
 {
     BOOST_REQUIRE_EQUAL("Whoa, chill out!", bob::hey("WHAT THE HELL WERE YOU THINKING?"));
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(shouting_numbers)
 {
     BOOST_REQUIRE_EQUAL("Whoa, chill out!", bob::hey("1, 2, 3 GO!"));
