aa4d96bb124f862207b974778528aee219d9c6a0
Boris Egorov <egorov@linux.com>
2015-12-04 01:10:25 +0600

    rust/anagram: Simplify code using closure

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,11 +1,11 @@
 fn is_anagram(a: &str, b: &str) -> bool {
-    let mut chars_a : Vec<char> = a.to_lowercase().chars().collect();
-    chars_a.sort();
+    let sorted_chars = |s: &str| {
+        let mut chars : Vec<char> = s.to_lowercase().chars().collect();
+        chars.sort();
+        chars
+    };
 
-    let mut chars_b : Vec<char> = b.to_lowercase().chars().collect();
-    chars_b.sort();
-
-    *chars_a == *chars_b && a != b
+    *sorted_chars(a) == *sorted_chars(b) && a != b
 }
 
 pub fn anagrams_for<'a>(word: &str, inputs: &'a [&'a str]) -> Vec<&'a str> {
