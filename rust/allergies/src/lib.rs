5cb4fb5e350c7643c559a0c8d3c822bc0ccfd902
Boris Egorov <egorov@linux.com>
2015-12-05 22:55:17 +0600

    rust/allergies: Pass test 1 (no allergies)

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, Eq, PartialEq)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+pub struct Allergies(pub usize);
+
+impl Allergies {
+    pub fn is_allergic_to(&self, _: &Allergen) -> bool {
+        false
+    }
+    pub fn allergies(&self) -> Vec<Allergen> {
+        vec![]
+    }
+}
