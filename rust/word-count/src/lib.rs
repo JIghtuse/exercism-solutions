a4b279229e7dda8832d5bca64ba1eda4dbe04d12
Boris Egorov <egorov@linux.com>
2015-12-08 10:01:10 +0600

    rust/word-count: Pass test 2 (count one word)

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
 
-pub fn word_count(_: &str) -> HashMap<String, u32> {
-    HashMap::new()
+pub fn word_count(text: &str) -> HashMap<String, u32> {
+    let mut result = HashMap::new();
+    if text.chars().any(|x| x.is_alphabetic()) {
+        result.insert(text.to_owned(), 1);
+    }
+    result
 }
