b3a7637a99677aef0456c7bc57ac5beb86c4552a
Boris Egorov <egorov@linux.com>
2015-12-08 11:43:24 +0600

    rust/word-count: Simplify word splitting

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 pub fn word_count(text: &str) -> HashMap<String, u32> {
     let mut result = HashMap::new();
 
-    for word in text.split(|c: char| c.is_whitespace() || !c.is_alphanumeric()) {
+    for word in text.split(|c: char| !c.is_alphanumeric()) {
         if !word.is_empty() {
             let word = word.to_lowercase().to_owned();
             let counter = result.entry(word).or_insert(0);
