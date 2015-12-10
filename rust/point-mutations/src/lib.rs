873b073c9ea66f01b4903921731c9efaede18e17
Boris Egorov <egorov@linux.com>
2015-12-11 00:46:54 +0600

    rust/point-mutations: Pass remaining tests

--- rust/point-mutations/src/lib.rs
+++ rust/point-mutations/src/lib.rs
@@ -1,6 +1,11 @@
 pub fn hamming_distance(a: &str, b: &str) -> Result<i32, &'static str> {
-    Ok(a.chars()
-        .zip(b.chars())
-        .filter(|x| x.0 != x.1)
-        .count() as i32)
+    match a.len() == b.len() {
+        true => {
+            Ok(a.chars()
+                .zip(b.chars())
+                .filter(|x| x.0 != x.1)
+                .count() as i32)
+        }
+        false => Err("inputs of different length"),
+    }
 }
