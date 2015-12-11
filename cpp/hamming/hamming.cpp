f5254769cedb7e36d7b4fce411a24eca8a34b671
Boris Egorov <egorov@linux.com>
2015-12-11 13:11:28 +0600

    cpp/hamming: Pass test 1 (identical strands)

---
+++ cpp/hamming/hamming.cpp
@@ -0,0 +1,8 @@
+#include "hamming.h"
+
+using namespace std;
+
+size_t hamming::compute(const string& a, const string& b)
+{
+    return 0;
+}
---
+++ cpp/hamming/hamming.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace hamming {
+    size_t compute(const std::string& a, const std::string& b);
+}
