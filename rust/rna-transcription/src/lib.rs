e4ae719928f28e98e0e7af7250520f37d577ba28
Boris Egorov <egorov@linux.com>
2015-12-12 00:42:21 +0600

    rust/rna-transcription: Huge refactoring (enums)

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,34 +1,84 @@
+use std::mem;
+
+#[derive(Debug,PartialEq)]
+enum DnaNucleotide {
+    G = 71,
+    C = 67,
+    T = 84,
+    A = 65,
+}
+
+#[derive(Debug,PartialEq)]
+enum RnaNucleotide {
+    G = 71,
+    C = 67,
+    U = 85,
+    A = 65,
+}
+
 #[derive(Debug,PartialEq)]
 pub struct RibonucleicAcid {
-    strand: String,
+    strand: Vec<RnaNucleotide>,
+}
+
+impl From<String> for RibonucleicAcid {
+    fn from(s: String) -> RibonucleicAcid {
+        RibonucleicAcid { strand: unsafe { mem::transmute(s.into_bytes()) } }
+    }
+}
+
+impl Into<String> for RibonucleicAcid {
+    fn into(self) -> String {
+        String::from_utf8(unsafe { mem::transmute(self.strand) }).unwrap()
+    }
+}
+
+impl AsRef<str> for RibonucleicAcid {
+    fn as_ref(&self) -> &str {
+        unsafe { mem::transmute(&self.strand[..]) }
+    }
 }
 
 impl RibonucleicAcid {
-    pub fn new(strand: &str) -> RibonucleicAcid {
-        RibonucleicAcid { strand: strand.to_owned() }
-    }
-    pub fn as_ref(&self) -> &str {
-        self.strand.as_ref()
+    pub fn new<S>(strand: S) -> RibonucleicAcid
+        where S: Into<String>
+    {
+        RibonucleicAcid::from(strand.into())
     }
 }
 
 #[derive(Debug,PartialEq)]
 pub struct DeoxyribonucleicAcid {
-    strand: String,
+    strand: Vec<DnaNucleotide>,
+}
+
+impl From<String> for DeoxyribonucleicAcid {
+    fn from(s: String) -> DeoxyribonucleicAcid {
+        DeoxyribonucleicAcid { strand: unsafe { mem::transmute(s.into_bytes()) } }
+    }
+}
+
+impl Into<String> for DeoxyribonucleicAcid {
+    fn into(self) -> String {
+        String::from_utf8(unsafe { mem::transmute(self.strand) }).unwrap()
+    }
+}
+
+impl AsRef<str> for DeoxyribonucleicAcid {
+    fn as_ref(&self) -> &str {
+        unsafe { mem::transmute(&self.strand[..]) }
+    }
 }
 
 impl DeoxyribonucleicAcid {
-    pub fn new(strand: &str) -> DeoxyribonucleicAcid {
-        DeoxyribonucleicAcid { strand: strand.to_owned() }
+    pub fn new<S>(strand: S) -> DeoxyribonucleicAcid
+        where S: Into<String>
+    {
+        DeoxyribonucleicAcid::from(strand.into())
     }
 
     pub fn to_rna(&self) -> RibonucleicAcid {
-        RibonucleicAcid {
-            strand: self.strand
-                        .replace("G", "C")
-                        .replace("C", "G")
-                        .replace("T", "A")
-                        .replace("A", "U")
-        }
+        let s = self.as_ref().to_owned();
+        RibonucleicAcid::from(s)
     }
 }
