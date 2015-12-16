3392a988556ae4955d2ed979c51d26fc3975aac8
Boris Egorov <egorov@linux.com>
2015-12-16 22:42:16 +0600

    cpp/anagram: Pass 'false positive' test

--- cpp/anagram/anagram.cpp
+++ cpp/anagram/anagram.cpp
@@ -1,14 +1,32 @@
 #include "anagram.h"
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 namespace anagram {
 
+vector<char> sorted_chars(const string& s) {
+    vector<char> chars(s.length());
+    for (char c : s)
+        chars.push_back(c);
+    sort(chars.begin(), chars.end());
+    return chars;
+}
+
+bool is_anagram(const string& a, const string& b) {
+    if (a.length() != b.length())
+        return false;
+
+    return sorted_chars(a) == sorted_chars(b);
+}
+
 vector<string> anagram::matches(const initializer_list<const string> inputs)
 {
     vector<string> res;
     for (const string& input : inputs)
-        if (input.length() == word.length())
+        if (is_anagram(word, input))
             res.push_back(input);
     return res;
 }
--- cpp/anagram/anagram_test.cpp
+++ cpp/anagram/anagram_test.cpp
@@ -22,7 +22,6 @@ BOOST_AUTO_TEST_CASE(detects_simple_anagram)
     BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(does_not_detect_false_positives)
 {
     auto subject = anagram::anagram("galea");
@@ -32,6 +31,7 @@ BOOST_AUTO_TEST_CASE(does_not_detect_false_positives)
     BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(detects_multiple_anagrams)
 {
     auto subject = anagram::anagram("master");
