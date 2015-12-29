069952ca00644028974b1db68b21a21538aa5d9d
Boris Egorov <egorov@linux.com>
2015-12-29 23:12:53 +0600

    rust/transform: Pass remaining tests

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,14 +1,13 @@
 use std::collections::BTreeMap;
-use std::ascii::AsciiExt;
 
 pub fn transform(old: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
     let mut res: BTreeMap<String, i32> = BTreeMap::new();
     for i in old {
-        let mut s = String::new();
         for c in i.1 {
+            let mut s = String::new();
             s.extend(c.to_lowercase().chars());
+            res.insert(s, *i.0);
         }
-        res.insert(s, *i.0);
     }
     res
 }
