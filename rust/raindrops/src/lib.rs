03a5778b95d8c8a075357da8d42ec323f7e3fd56
Boris Egorov <egorov@linux.com>
2015-12-29 23:31:50 +0600

    rust/raindrops: Pass 3rd test (has 5 factor)

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,6 +1,7 @@
 pub fn raindrops(number: i32) -> String {
-    match number % 3 {
-        0 => "Pling".to_string(),
+    match (number % 3, number % 5) {
+        (0, _) => "Pling".to_string(),
+        (_, 0) => "Plang".to_string(),
         _ => number.to_string(),
     }
 }
