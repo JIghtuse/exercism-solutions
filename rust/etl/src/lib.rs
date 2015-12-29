40a086b7e446f1f920b03569c362d014eae12ddd
Boris Egorov <egorov@linux.com>
2015-12-29 23:14:45 +0600

    rust/transform: Simplify code

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -3,10 +3,8 @@ use std::collections::BTreeMap;
 pub fn transform(old: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
     let mut res: BTreeMap<String, i32> = BTreeMap::new();
     for i in old {
-        for c in i.1 {
-            let mut s = String::new();
-            s.extend(c.to_lowercase().chars());
-            res.insert(s, *i.0);
+        for s in i.1 {
+            res.insert(s.to_lowercase(), *i.0);
         }
     }
     res
