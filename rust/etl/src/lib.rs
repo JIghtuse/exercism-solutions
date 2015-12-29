9dd0d676b2c735c3dd91abe2d4879f96d02ee47f
Boris Egorov <egorov@linux.com>
2015-12-29 23:17:58 +0600

    rust/transform: Better names

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -2,9 +2,9 @@ use std::collections::BTreeMap;
 
 pub fn transform(old: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
     let mut res: BTreeMap<String, i32> = BTreeMap::new();
-    for i in old {
-        for s in i.1 {
-            res.insert(s.to_lowercase(), *i.0);
+    for (score, words) in old {
+        for word in words {
+            res.insert(word.to_lowercase(), *score);
         }
     }
     res
