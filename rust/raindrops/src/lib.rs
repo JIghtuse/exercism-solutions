253607ce08144f4dc455b26d579750221b761b40
Boris Egorov <egorov@linux.com>
2016-01-12 22:49:34 +0600

    rust/raindrops: [clippy] Use to_owned() where possible

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn raindrops(number: i32) -> String {
     match (number % 3, number % 5, number % 7) {
-        (0, 0, 0) => "PlingPlangPlong".to_string(),
-        (0, 0, _) => "PlingPlang".to_string(),
-        (0, _, 0) => "PlingPlong".to_string(),
-        (_, 0, 0) => "PlangPlong".to_string(),
-        (0, _, _) => "Pling".to_string(),
-        (_, 0, _) => "Plang".to_string(),
-        (_, _, 0) => "Plong".to_string(),
+        (0, 0, 0) => "PlingPlangPlong".to_owned(),
+        (0, 0, _) => "PlingPlang".to_owned(),
+        (0, _, 0) => "PlingPlong".to_owned(),
+        (_, 0, 0) => "PlangPlong".to_owned(),
+        (0, _, _) => "Pling".to_owned(),
+        (_, 0, _) => "Plang".to_owned(),
+        (_, _, 0) => "Plong".to_owned(),
         _ => number.to_string(),
     }
 }
