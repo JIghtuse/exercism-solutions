c328bdc7748d0844b947b379c69390ea375b5698
Boris Egorov <egorov@linux.com>
2016-01-12 22:48:46 +0600

    rust/point-mutations: [clippy] Use if instead of match

--- rust/point-mutations/src/lib.rs
+++ rust/point-mutations/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn hamming_distance(strand_a: &str, strand_b: &str) -> Result<usize, &'static str> {
-    match strand_a.len() == strand_b.len() {
-        true => {
-            Ok(strand_a.chars()
-                       .zip(strand_b.chars())
-                       .filter(|&(a, b)| a != b)
-                       .count())
-        }
-        false => Err("inputs of different length"),
+    if strand_a.len() == strand_b.len() {
+        Ok(strand_a.chars()
+                   .zip(strand_b.chars())
+                   .filter(|&(a, b)| a != b)
+                   .count())
+    } else {
+        Err("inputs of different length")
     }
 }
