8800afe395114082c057789d8fc629c68f93e01c
Boris Egorov <egorov@linux.com>
2016-02-04 22:20:58 +0600

    cpp/rna_transcription: Add skeleton

---
+++ cpp/rna-transcription/rna_transcription.cpp
@@ -0,0 +1,6 @@
+#include "rna_transcription.h"
+
+char transcription::to_rna(char c)
+{
+    return c;
+}
---
+++ cpp/rna-transcription/rna_transcription.h
@@ -0,0 +1,8 @@
+#ifndef RNA_TRANSCRIPTION_H
+#define RNA_TRANSCRIPTION_H
+
+namespace transcription {
+    char to_rna(char c);
+}
+
+#endif // RNA_TRANSCRIPTION_H
