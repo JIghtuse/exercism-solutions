2f51083aec7a0f1d6789aac009029c3c28272d4f
Boris Egorov <egorov@linux.com>
2016-02-04 19:45:55 +0600

    cpp/beer-song: Pass first test
    Go TDD-style: only needed code

--- cpp/beer-song/beer_song.cpp
+++ cpp/beer-song/beer_song.cpp
@@ -1,8 +1,13 @@
+#include <sstream>
 #include "beer_song.h"
 
 using namespace std;
 
 string beer::verse(size_t)
 {
-    return "";
+    ostringstream out;
+    out << "8 bottles of beer on the wall, 8 bottles of beer.\n";
+    out << "Take one down and pass it around, 7 bottles of beer on the wall.\n";
+
+    return out.str();
 }
