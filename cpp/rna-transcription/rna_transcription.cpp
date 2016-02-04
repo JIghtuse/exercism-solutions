b40a43d23ba4eb43bb594ccec3843686dafcf94d
Boris Egorov <egorov@linux.com>
2016-02-04 22:22:45 +0600

    cpp/rna_transcription: Pass 'C' -> 'G' test

--- cpp/rna-transcription/rna_transcription.cpp
+++ cpp/rna-transcription/rna_transcription.cpp
@@ -1,6 +1,14 @@
+#include <stdexcept>
 #include "rna_transcription.h"
+
+using namespace std;
 
 char transcription::to_rna(char c)
 {
-    return c;
+    switch (c) {
+    case 'C':
+        return 'G';
+    default:
+        throw invalid_argument{"No such nucleotide"};
+    }
 }
