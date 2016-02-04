2124c74a227e979762427af23d8f7b1a302799d1
Boris Egorov <egorov@linux.com>
2016-02-04 22:24:29 +0600

    cpp/rna_transcripton: Pass more char->char conversion tests

--- cpp/rna-transcription/rna_transcription.cpp
+++ cpp/rna-transcription/rna_transcription.cpp
@@ -8,6 +8,12 @@ char transcription::to_rna(char c)
     switch (c) {
     case 'C':
         return 'G';
+    case 'G':
+        return 'C';
+    case 'A':
+        return 'U';
+    case 'T':
+        return 'A';
     default:
         throw invalid_argument{"No such nucleotide"};
     }
