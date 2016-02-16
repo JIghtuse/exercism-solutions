4db7dfcb54dad2e5c5efc7c501176aea25bb06a2
Boris Egorov <egorov@linux.com>
2016-02-16 09:17:12 +0600

    python/RLE: Add solution

---
+++ python/run-length-encoding/run_length.py
@@ -0,0 +1,17 @@
+import re
+from itertools import groupby
+
+def encode(s):
+    res = ""
+    for char, group in groupby(s):
+        length = len(list(group))
+        if length > 1:
+            res += str(length)
+        res += char
+    return res
+
+def decode(s):
+    nums = (int(n) if n else 1 for n in re.split('\D', s))
+    chars = ''.join(n for n in re.split('\d', s) if n)
+
+    return ''.join(char * number for (number, char) in zip(nums, chars))
