4d08e894f684c220f82970efc286b76ec14565d8
Boris Egorov <egorov@linux.com>
2016-03-16 11:15:41 +0600

    cpp/beer-song: Fix review notes

--- cpp/beer-song/beer_song.cpp
+++ cpp/beer-song/beer_song.cpp
@@ -1,35 +1,36 @@
-#include <sstream>
 #include "beer_song.h"
+#include <sstream>
 
 using namespace std;
 
 struct Bottles {
+    explicit Bottles(size_t n, const std::string& what="beer")
+        : what{what}
+        , n{n}
+    {}
+    const char* one() const { return n == 1 ? "it" : "one"; }
+    void stream(std::ostream& out, bool capitalized) const
+    {
+        switch (n) {
+        case 0:
+            out << (capitalized ? 'N' : 'n') << "o more bottles of " << what;
+            break;
+        case 1:
+            out << "1 bottle of " << what;
+            break;
+        default:
+            out << n << " bottles of " << what;
+            break;
+        }
+    }
+    std::string what;
     size_t n;
 };
 
 ostream& operator<<(ostream& out, const struct Bottles& bottles)
 {
-    switch (bottles.n) {
-    case 0:
-        out << "no more bottles of beer";
-        break;
-    case 1:
-        out << "1 bottle of beer";
-        break;
-    default:
-        out << bottles.n << " bottles of beer";
-        break;
-    }
+    bottles.stream(out, false);
     return out;
-}
-
-string capitalize(const struct Bottles& bottles)
-{
-    ostringstream out;
-    out << bottles;
-    auto result = out.str();
-    result[0] = toupper(result[0]);
-    return result;
 }
 
 string beer::verse(size_t n)
@@ -37,14 +38,14 @@ string beer::verse(size_t n)
     auto bottles = Bottles{n};
 
     ostringstream out;
-    out << capitalize(bottles) << " on the wall, " << bottles << ".\n";
+    bottles.stream(out, true);
+    out << " on the wall, " << bottles << ".\n";
 
     if (!bottles.n)
         out << "Go to the store and buy some more, "
             << Bottles{99} << " on the wall.\n";
     else
-        out << "Take " << (bottles.n == 1 ? "it" : "one")
-            << " down and pass it around, "
+        out << "Take " << bottles.one() << " down and pass it around, "
             << Bottles{n - 1} << " on the wall.\n";
 
     return out.str();
@@ -54,7 +55,7 @@ string beer::sing(size_t from, size_t down_to)
 {
     ostringstream out;
     size_t i;
-    for (i = from; i != down_to; --i)
+    for (i = from; i > down_to; --i)
         out << verse(i) << '\n';
     out << verse(i);
     return out.str();
