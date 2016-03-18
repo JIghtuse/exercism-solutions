173304882b570ccc1be9af24f696b95d27c3c358
Boris Egorov <egorov@linux.com>
2016-03-18 12:56:07 +0600

    rust/hamming: Pass remaining tests

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,4 +1,8 @@
 pub fn hamming_distance(a: &str, b: &str) -> Result<usize, &'static str> {
+    if a.len() != b.len() {
+        return Err("inputs of different length");
+    }
+
     Ok(a.chars()
         .zip(b.chars())
         .filter(|&(i, j)| i != j)
