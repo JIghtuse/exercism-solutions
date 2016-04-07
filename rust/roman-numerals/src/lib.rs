8e58ec0af3ac08886c55b52f8337deb7d9c413e0
Boris Egorov <egorov@linux.com>
2016-04-08 00:39:23 +0600

    rust/roman-numerals: Complete exercise

--- rust/roman-numerals/src/lib.rs
+++ rust/roman-numerals/src/lib.rs
@@ -13,8 +13,33 @@ impl<'a> PartialEq<Roman> for &'a str {
     }
 }
 
-impl From<usize> for Roman {
-    fn from(_: usize) -> Roman {
-        Roman { repr: "I".to_string() }
+static ARABIC_ROMAN : [(i32, &'static str); 13] = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
+];
+
+impl From<i32> for Roman {
+    fn from(n: i32) -> Roman {
+        let mut n = n;
+        let mut repr = String::new();
+
+        for &(i, roman_literal) in ARABIC_ROMAN.into_iter() {
+            while n >= i {
+                n -= i;
+                repr.push_str(roman_literal);
+            }
+        }
+        Roman { repr: repr }
     }
 }
