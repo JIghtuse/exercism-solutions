1ff9c92b49dd50c84ca52ea60c9655e948ed037a
Boris Egorov <egorov@linux.com>
2016-04-08 13:58:30 +0600

    rust/sieve: Complete solution

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -2,6 +2,13 @@ pub struct Sieve;
 
 impl Sieve {
     pub fn primes_up_to(n: u32) -> Vec<u32> {
-        (2 .. n + 1).collect()
+        let mut result = (2 .. n + 1).collect::<Vec<u32>>();
+        let mut pos = 0;
+        while pos != result.len() {
+            let prime = result[pos];
+            result.retain(|&i| i == prime || i % prime != 0);
+            pos += 1;
+        }
+        result
     }
 }
--- rust/sieve/tests/sieve.rs
+++ rust/sieve/tests/sieve.rs
@@ -13,20 +13,17 @@ fn limit_is_the_first_prime() {
 }
 
 #[test]
-#[ignore]
 fn primes_up_to_10() {
     assert_eq!(Sieve::primes_up_to(10), [2, 3, 5, 7]);
 }
 
 
 #[test]
-#[ignore]
 fn limit_is_prime() {
     assert_eq!(Sieve::primes_up_to(13), [2, 3, 5, 7, 11, 13]);
 }
 
 #[test]
-#[ignore]
 fn limit_of_1000() {
     let expected = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
                         71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149,
