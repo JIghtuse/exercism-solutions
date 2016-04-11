fe5c4aae5d97784f2ae4722b60245f64b65de9fa
Boris Egorov <egorov@linux.com>
2016-04-11 22:48:15 +0600

    cpp/series: Pass all 'digits' tests

--- cpp/series/series_test.cpp
+++ cpp/series/series_test.cpp
@@ -15,7 +15,6 @@ BOOST_AUTO_TEST_CASE(short_digits)
     BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), actual.begin(), actual.end());
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(long_digits)
 {
     const vector<int> expected{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -43,6 +42,7 @@ BOOST_AUTO_TEST_CASE(keeps_arbitrary_digit_order)
     BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), actual.begin(), actual.end());
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(can_slice_by_1)
 {
     const vector<vector<int>> expected{{0}, {1}, {2}, {3}, {4}};
