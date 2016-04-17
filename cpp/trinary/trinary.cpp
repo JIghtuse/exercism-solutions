3cf243cbb6ddade7648c839216492cd669c3ad6f
Boris Egorov <egorov@linux.com>
2016-04-17 22:36:32 +0600

    cpp/trinary: Project skeleton

---
+++ cpp/trinary/trinary.cpp
@@ -0,0 +1,6 @@
+#include "trinary.h"
+
+int trinary::to_decimal(std::experimental::string_view sv)
+{
+    return 1;
+}
---
+++ cpp/trinary/trinary.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <experimental/string_view>
+
+namespace trinary {
+
+int to_decimal(std::experimental::string_view);
+
+} // namespace trinary
