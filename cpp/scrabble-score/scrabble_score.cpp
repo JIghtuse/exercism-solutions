0c12a7a87054cda3f004d21d897b4118965cf4b9
Boris Egorov <egorov@linux.com>
2016-04-22 22:06:32 +0600

    cpp/scrabble_score: Project skeleton

---
+++ cpp/scrabble-score/scrabble_score.cpp
@@ -0,0 +1,5 @@
+#include "scrabble_score.h"
+
+int scrabble_score::score(std::experimental::string_view word)
+{
+}
---
+++ cpp/scrabble-score/scrabble_score.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <experimental/string_view>
+
+namespace scrabble_score {
+
+int score(std::experimental::string_view word);
+
+} // namespace scrabble_score
