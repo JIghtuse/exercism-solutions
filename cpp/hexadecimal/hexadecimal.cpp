8c830a41be830ec1013e484984fed3ca3321931d
Boris Egorov <egorov@linux.com>
2016-04-23 14:47:51 +0600

    cpp/hexadecimal: Project skeleton

---
+++ cpp/hexadecimal/hexadecimal.cpp
@@ -0,0 +1,6 @@
+#include "hexadecimal.h"
+
+int hexadecimal::convert(std::experimental::string_view sv)
+{
+    return 1;
+}
---
+++ cpp/hexadecimal/hexadecimal.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <experimental/string_view>
+
+namespace hexadecimal {
+
+int convert(std::experimental::string_view sv);
+
+} // namespace hexadecimal
