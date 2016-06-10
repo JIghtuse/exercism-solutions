d1b776b889302ef8d31380e8c7845636abea633c
Boris Egorov <egorov@linux.com>
2016-06-10 10:55:46 +0600

    rust/pangram: Complete solution

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,3 +1,16 @@
-pub fn is_pangram(_: &str) -> bool {
-    false
+use std::collections::HashSet;
+
+const ALPHABET_LENGTH: usize = 26;
+
+fn is_letter(c: &char) -> bool {
+    let c = *c as u8;
+    b'a' <= c && c <= b'z'
+}
+
+pub fn is_pangram(s: &str) -> bool {
+    let letters: HashSet<char> = s.to_lowercase()
+        .chars()
+        .filter(is_letter)
+        .collect();
+    letters.len() == ALPHABET_LENGTH
 }
