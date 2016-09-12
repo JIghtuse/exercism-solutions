f3174c71963e276ca8e3ad61593e073bdf28da8e
Boris Egorov <egorov@linux.com>
2016-09-12 23:37:45 +0700

    cpp/sum_of_multiples: Project skeleton

---
+++ cpp/sum-of-multiples/sum_of_multiples.cpp
@@ -0,0 +1,6 @@
+#include "sum_of_multiples.h"
+
+unsigned sum_of_multiples::to(unsigned n)
+{
+    return 0;
+}
---
+++ cpp/sum-of-multiples/sum_of_multiples.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace sum_of_multiples {
+
+unsigned to(unsigned n);
+
+} // namespace sum_of_multiples
