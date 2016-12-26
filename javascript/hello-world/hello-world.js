d5c247fbd960cd061003b903b8f737655a9ded31
Boris Egorov <egorov@linux.com>
2016-12-26 23:40:35 +0700

    js/hello-world: Add solution

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,14 @@
+//
+// This is a stub file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at hello-world.spec.js--that should give you some hints about what is
+// expected here.
+
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+  let who = input || "World";
+  return `Hello, ${who}!`;
+};
+
+module.exports = HelloWorld;
